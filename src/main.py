bd156b0ccd800ba40071610e3f293531ea367f0c
MasoumehTafvizi <masoumeh.tafviziii@gmail.com>
2026-02-25 18:34:11 +0100

    Implementing the main funtions for currency converter

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_exchange_rate(base_currency, target_currency):
+    url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
+    response = requests.get(url)
+    return response.json().get('rates').get(target_currency)
+
+def convert_currency(amount, exchange_rate):
+    return amount * exchange_rate
+
+if __name__ == "__main__":
+    base_currency = input("Enter base currency (e.g., USD): ")
+    target_currency = input("Enter target currency (e.g., CAD): ")
+    base_amount = float(input(f"Enter amount in {base_currency}: "))
+    
+    exchange_rate = get_exchange_rate(base_currency, target_currency)
+    targetted_amount = convert_currency(base_amount, exchange_rate)
+    
+    print(f"{base_amount} {base_currency} is : {targetted_amount} {target_currency}") 
